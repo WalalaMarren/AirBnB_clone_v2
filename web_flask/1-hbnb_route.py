0d0bf7383d8c75b2caa84bf4ae20e5aa12590d1d
WalalaMarren <marrenanyangonyagaya@gmail.com>
2023-09-22 10:24:26 +0000

    add route

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -9,10 +9,12 @@ def hello_hbnb():
     """Return Hello HBNB"""
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """functions to return hbnb"""
+    """Return HBNB"""
     return 'HBNB'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
