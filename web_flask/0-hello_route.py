f4be50875ee3ecfac25add920a52b420c346c077
WalalaMarren <marrenanyangonyagaya@gmail.com>
2023-09-22 10:17:25 +0000

    start flask app

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,15 +1,14 @@
-#!/usr/bin/python
-#Importing flask and starting up an application
+#!/usr/bin/python3
+"""what the program does"""
 from flask import Flask
 app = Flask(__name__)
-#A script that starts a flask application
-#Your web application must be listening on 0.0.0.0, port 5000
 
-@app.route('/')
-def hbn():
-    #function to return HBNB string
-    return “Hello HBNB!”
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Return Hello HBNB"""
+    return 'Hello HBNB!'
+
 
 if __name__ == '__main__':
-    #enables debugging
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
